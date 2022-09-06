65f132796b555d14f483d2b2afc7073ec4633861
sonwil1203 <sonwil12@hotmail.com>
2022-09-05 23:12:13 -0500

    Migracion tabla_7 en capa persistencia

---
+++ persistencia/Migrations/20220906034813_tablas_7.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace persistencia.Migrations
+{
+    public partial class tablas_7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "camionetas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Platon = table.Column<bool>(type: "bit", nullable: false),
+                    Transmision = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    placa_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_camionetas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Factura_id = table.Column<int>(type: "int", nullable: false),
+                    Identificaion_id = table.Column<int>(type: "int", nullable: false),
+                    placa_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Facturas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cliente_id = table.Column<int>(type: "int", nullable: false),
+                    Tecnico_id = table.Column<int>(type: "int", nullable: false),
+                    Placa_id = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    filtro_aire = table.Column<bool>(type: "bit", nullable: false),
+                    filtro_Gasolina = table.Column<bool>(type: "bit", nullable: false),
+                    Cambio_aceite = table.Column<bool>(type: "bit", nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facturas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tecnicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Especialidad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Identificaion_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tecnicos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cilindraje = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_modelo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    tipo_combustible = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehiculos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "camionetas");
+
+            migrationBuilder.DropTable(
+                name: "clientes");
+
+            migrationBuilder.DropTable(
+                name: "Facturas");
+
+            migrationBuilder.DropTable(
+                name: "tecnicos");
+
+            migrationBuilder.DropTable(
+                name: "vehiculos");
+        }
+    }
+}
