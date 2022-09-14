3408d583983b4c841cdddb5d5853fe1dbc7c6ac6
kevingomez369 <kevin.gomez98@hotmail.com>
2022-09-13 22:15:02 -0500

    ..

--- Frontend/Pages/Personas/Update.cshtml.cs
+++ Frontend/Pages/Personas/Update.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio;
+using persistencia;
 
 namespace MyApp.Namespace
 {
     public class UpdateModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+       private  readonly IRepositorioPersona _repo;
+       public Persona Persona { get; set; }
+       public UpdateModel(IRepositorioPersona repositorio){
+       _repo = repositorio;
+       }
+       public void OnGet()
+       {
+        
+
+       }
+       public IActionResult OnPost(Persona persona){
+       _repo.Update(persona);
+       
+       return new RedirectToPageResult("/Personas/List");
+      }
+
     }
 }
