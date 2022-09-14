2de407ad5ba5329ac8a6e8839f8f7f9668e798c3
sonwil1203 <sonwil12@hotmail.com>
2022-09-13 21:44:29 -0500

    agrago campos requeridos en capa frontend persona

--- Dominio/Persona.cs
+++ Dominio/Persona.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dominio
 {
     public class Persona
     {
-        public int Id { get; set; }
+        //[Key]
+        public int Id { get; set; } //CedulaId
+        [Required(ErrorMessage="El campo Nombres es requerido"),Display(Name="Nombres"), StringLength(50)]
         public string Nombres { get; set; }
+        [Required(ErrorMessage="El campo Apellidos es requerido"),StringLength(50)]
         public string Apellidos { get; set; }
+        [Required(ErrorMessage="El campo Direccion es requerido"),StringLength(80)]
         public string Direccion { get; set; }
+        [Required(ErrorMessage="El campo Celular es requerido"),StringLength(10)]
+        [RegularExpression("[0-9]*$",ErrorMessage="Digite solo numeros")]
         public string Celular { get; set; }
       
       
